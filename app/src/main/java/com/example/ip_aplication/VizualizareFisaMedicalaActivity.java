67ce95ed906bccd3fbdf56be653f7beaa8a301e0
ligia1002 <andrei10022002lidia@yahoo.com>
2024-05-26 22:23:37 +0300

    Commit

---
+++ app/src/main/java/com/example/ip_aplication/VizualizareFisaMedicalaActivity.java
@@ -0,0 +1,14 @@
+package com.example.ip_aplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class VizualizareFisaMedicalaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vizualizare_fisa_medicala); // Referință către layout-ul tău
+    }
+}
